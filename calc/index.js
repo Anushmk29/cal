0cc8272f408de0bfd3230fdf196174c2b31dd27f
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-24 12:28:21 +0000

    input from Keyboard Added

--- calc/index.js
+++ calc/index.js
@@ -1,34 +1,40 @@
 let inputBox = document.getElementById('inputBox')
-let buttons = document.querySelectorAll('button')
-
+let buttons = Array.from(document.querySelectorAll('button'))
 let string = ''
 
-buttons.forEach(ele=>{
-    ele.addEventListener('click',(b)=>{
-        if(b.target.innerText == '=')
-        {
-            string= String(eval(string))
-            inputBox.value = string;
-        }
-        else if(b.target.innerText == 'AC')
-        {
-            string = ''
-            inputBox.value = string
-        }
-        else if(b.target.innerText == 'DEL')
-        {
-             string = string.substring(0,string.length-1)
-             inputBox.value = string
-        }
-        else if(b.target.id == 'plus-minus')
-        {
-            string = String(-eval(string))
-            inputBox.value = string
-
-        }
-        else{
-            string += b.target.innerText
-            inputBox.value = string
-        }
+buttons.forEach(ele => {
+    ele.addEventListener('click', (b) => {
+        handleInput(b.target.innerText);
     })
 })
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+    const buttonKeys = buttons.map(button => button.innerText);
+    if (buttonKeys.includes(key)) {
+        handleInput(key);
+    }
+})
+
+function handleInput(input) {
+    if(input == '=') {
+        string = String(eval(string))
+        inputBox.value = string;
+    }
+    else if(input == 'AC') {
+        string = ''
+        inputBox.value = string
+    }
+    else if(input == 'DEL') {
+        string = string.substring(0,string.length-1)
+        inputBox.value = string
+    }
+    else if(input == '±') {
+        string = String(-eval(string))
+        inputBox.value = string
+    }
+    else {
+        string += input
+        inputBox.value = string
+    }
+}
